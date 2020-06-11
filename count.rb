1324d9a5b081ecb4c7294a25c0427db6373e6da8
phbasin <pierre-henri.basin@orange.fr>
2020-06-11 16:30:30 +0200

    two_sum method added

--- count.rb
+++ count.rb
@@ -1,5 +1,6 @@
 # count_occurences([2, -1, 1, 1, -1]) => {1: 2, 2: 1, -1: 2}
 
+# 1
 def count_occurrences(array)
   hash = {}
   array.uniq.each do |number|
@@ -8,6 +9,7 @@ def count_occurrences(array)
   puts hash
 end
 
+# 2
 def count_occurences_long(array)
   hash = {}
   array.each do |number|
@@ -20,8 +22,23 @@ def count_occurences_long(array)
   puts hash
 end
 
+# 3
 def count_occurences_simple(array)
   hash = Hash.new(0)
   array.each { |number| hash[number] += 1 }
   puts hash
+end
+
+# two_sum
+
+def two_sum(array, target)
+  hash = {}
+  array.each_with_index { |number, index| hash[number] = index }
+
+  array.each_with_index do |number, index|
+    #  two_sums([5], 10) => [0, 0]
+    if hash[target - number] && index != hash[target - number]
+      return [index, hash[target - number]]
+    end
+  end
 end
